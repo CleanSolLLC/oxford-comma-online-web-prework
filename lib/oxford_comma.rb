9dedf153499289000800641ed8189c10d45de9bf
Mark May <mamark90@gmail.com>
2019-03-30 15:11:00 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,8 @@
 def oxford_comma(array)
-
-new_array1 = array.join(" ")
-
+  new_array1 = array.join(" ")
 end
 
 def oxford_comma(array)
-
-#and_string = "and"
-new_array2 = array.join(" ")
-
+  new_array[-1] = array.push("and")
+  new_array2 = array.join(" ")
 end
