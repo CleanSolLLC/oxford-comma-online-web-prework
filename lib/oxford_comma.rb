bc6388971ecd7b406ccb6e98179e6a7cfb097007
Mark May <mamark90@gmail.com>
2019-03-30 17:21:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,17 +5,20 @@ def oxford_comma(array)
   array_last_item = array.last
   index_num = array.find_index(array_last_item)
 
-  if index_num > 0 #more than 1 element in array
+  if index_num > 1 #more than 2 elements in array
   array.pop
   string = "and " + "#{array_last_item}"
   array.insert(index_num, string)
   new_array = array.join("," " ")
 
-elsif index_num == 0 && array[0] != ""
+elsif index_num == 1 && array[0,1] != "" #2 elements in array
+  string = "and " + "#{array_last_item}"
+  array.pop
+  array.insert(index_num, string)
   new_array = array.join(" ")
 
-else puts "No elements in array!"
-
+elsif index_num == 0 && array[0] != ""
+  new_array = array.join(" ") 
 end
 
 end
