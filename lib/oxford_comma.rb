ce411311a5374d19f7dbd698a82245e067aaf633
Mark May <mamark90@gmail.com>
2019-03-30 16:43:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,13 @@
 def oxford_comma(array)
   new_array1 = array.join(" ")
-#end
 
-#def oxford_comma(array)
+#  array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
+  array_last_item = array.last
+  index_num = array.find_index(array_last_item)
+  array.pop
+  string = "and " + "#{array_last_item}"
+  array.insert(index_num, string)
   new_array2 = array.join("," " ")
 
 end
 
-  array = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
-  array_last_item = array.last
-  index_num = array.find_index(array_last_item)
-  array.insert(index_num, "and")
-
-  push string "and" at the position of the second to last element of the array
